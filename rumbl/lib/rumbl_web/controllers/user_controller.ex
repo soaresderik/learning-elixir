cdce9d984287ac571e4941d84e215b70b37fe448
andre.ferreira <andreferreira.sh@gmail.com>
2021-04-14 19:33:41 -0300

    login strategy p.83

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -3,6 +3,9 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Repo
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
+
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render(conn, "index.html", users: users)
@@ -19,15 +22,27 @@ defmodule RumblWeb.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
-
+    changeset = User.registration_changeset(%User{}, user_params)
+    IO.inspect changeset
     case Repo.insert(changeset) do
      {:ok, user} ->
         conn
+          |> Rumbl.Auth.login(user)
           |> put_flash(:info, "#{user.name} criado com sucesso!")
           |> redirect(to: Routes.user_path(conn, :index))
      {:error, changeset } ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "Você precisa estar logado para acessar a página")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt
     end
   end
 end
--- rumbl/lib/rumbl_web/models/user.ex
+++ rumbl/lib/rumbl_web/models/user.ex
@@ -14,8 +14,25 @@ defmodule Rumbl.User do
 
   def changeset(%User{} = model, params) do
     model
-    |> cast(params, [:name, :username])
+    |> cast(params, [:username, :name])
     |> validate_required([:username, :name])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(%User{} = model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 36)
+    |> put_pass_hash
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ -> changeset
+    end
   end
 end
